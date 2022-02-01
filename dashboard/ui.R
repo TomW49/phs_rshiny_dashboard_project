9077da1df0e1ff26143d8c786d9d27d800275312
Neringa4 <94367447+Neringa4@users.noreply.github.com>
2022-02-01 14:18:53 +0000

    Update ui.R

--- dashboard/ui.R
+++ dashboard/ui.R
@@ -5,4 +5,38 @@ ui <- fluidPage(
   
   #tags$h1 makes header
   titlePanel(tags$h1("Winter's effect on the NHS")),
+
+  fluidRow(
+    column(
+      
+    ),
+    column(
+      
+    ),
+    column(
+      
+    )
+  ),
+  
+  fluidRow(
+    column(
+      fluidRow(
+        
+      ),
+      fluidRow(
+        
+      )
+    ),
+    column(
+      fluidRow(
+        
+      ),
+      fluidRow(
+        plotOutput(outputId = "capacity_plot")
+      )
+    ),
+    column(
+      
+    )
+  )
 )
