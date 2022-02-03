7225c91dee0115b96f9e4151142d267855483a3b
Neringa4 <94367447+Neringa4@users.noreply.github.com>
2022-02-03 11:59:54 +0000

    Update titles

--- dashboard/server.R
+++ dashboard/server.R
@@ -14,7 +14,7 @@ server <- function(input, output) {
       labs(
         x = "Quarter",
         y = "Occupancy",
-        title = "Quarterly Hospital Occupancy (2016 - 2021)"
+        title = "Figure 2: Quarterly Hospital Occupancy (2016 - 2021)"
       ) +
       theme_minimal() +
       theme(axis.text.x = element_text(angle = 45, hjust = 1),
@@ -53,7 +53,7 @@ server <- function(input, output) {
           x = "Quarter and Year",
           y = "Count of individuals in each SIMD",
           colour = "SIMD",
-          title = "The count of individuals in each SIMD across 2016 Q2 -2021 Q2",
+          title = "Figure 1: The count of individuals in each SIMD across 2016 Q2 -2021 Q2",
           subtitle = "Deprivation levels: 1(Most Deprived) - 5(Least Deprived)"
         ) +
         theme_light() +
@@ -63,40 +63,26 @@ server <- function(input, output) {
               plot.subtitle = element_text(colour = "grey25"))
     } else {
       simd_quarter %>%
-      filter(!is.na(simd)) %>%
-      group_by(quarter, admission_type) %>%
-      count(simd) %>% 
-      filter(admission_type == input$admission_input) %>%
-      ggplot(aes(x = quarter, y = n, colour = as.factor(simd), group = simd)) +
-      geom_line() +
-      scale_color_manual(values = cbbPalette) +
-      labs(
-        x = "Quarter and Year",
-        y = "Count of individuals in each SIMD",
-        colour = "SIMD",
-        title = "The count of individuals in each SIMD across 2016 Q2 -2021 Q2",
-        subtitle = "Deprivation levels: 1(Most Deprived) - 5(Least Deprived)"
-      ) +
-      theme_light() +
-      theme(axis.text.x = element_text(angle = 45, hjust = 1),
-            axis.title = element_text(colour = "grey15"),
-            plot.title = element_text(colour = "grey25"),
-            plot.subtitle = element_text(colour = "grey25"))
+        filter(!is.na(simd)) %>%
+        group_by(quarter, admission_type) %>%
+        count(simd) %>% 
+        filter(admission_type == input$admission_input) %>%
+        ggplot(aes(x = quarter, y = n, colour = as.factor(simd), group = simd)) +
+        geom_line() +
+        scale_color_manual(values = cbbPalette) +
+        labs(
+          x = "Quarter and Year",
+          y = "Count of individuals in each SIMD",
+          colour = "SIMD",
+          title = "Figure 1: The count of individuals in each SIMD across 2016 Q2 -2021 Q2",
+          subtitle = "Deprivation levels: 1(Most Deprived) - 5(Least Deprived)"
+        ) +
+        theme_light() +
+        theme(axis.text.x = element_text(angle = 45, hjust = 1),
+              axis.title = element_text(colour = "grey15"),
+              plot.title = element_text(colour = "grey25"),
+              plot.subtitle = element_text(colour = "grey25"))
     }
-  })
-
-  output$age_plot <- renderPlot({
-    
-    demographics_age %>% 
-      ggplot(aes(x = age, y = total_stays)) +
-      geom_col(show.legend = FALSE) +
-      labs(
-        x = "Age (Years)",
-        y = "Total Stays",
-        title = "Hospitalisations period ber age"
-      ) +
-      theme_minimal() +
-      theme(axis.text.x = element_text(angle = 45, hjust = 1))
   })
   
   output$sex_plot <- renderPlot({
@@ -108,8 +94,24 @@ server <- function(input, output) {
       labs(
         x = "Sex",
         y = "Total Stays",
-        title = "Hospitalisations Period ber Gender"
+        title = "Figure 3: Winter Hopsitalisations by Sex"
       ) +
       theme_minimal()
   })
+  
+  output$age_plot <- renderPlot({
+    
+    demographics_age %>% 
+      ggplot(aes(x = age, y = total_stays)) +
+      geom_col(show.legend = FALSE) +
+      labs(
+        x = "Age (Years)",
+        y = "Total Stays",
+        title = "Figure 4: Winter Hospitalisations by Age Group"
+      ) +
+      theme_minimal() +
+      theme(axis.text.x = element_text(angle = 45, hjust = 1))
+  })
+  
+  
 }
