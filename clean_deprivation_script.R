bbc5af28e380e2b7813456ec610ceecf67d235a2
emilynoone <emilynoone@talktalk.net>
2022-02-03 15:11:24 +0000

    updated_Script

--- clean_deprivation_script.R
+++ clean_deprivation_script.R
@@ -3,8 +3,8 @@ library(tidyverse)
 library(ggplot2)
 
 
-#I am going to look at the deprivation data and look at the count of each deprivation 
-  deprivation_data <- read_csv(here::here("")
+#I am going to look at the deprivation data and look at the count of each deprivation
+deprivation_data <- read_csv(here::here("../raw_data/treatment_and_deprevation.csv"))
 deprivation_data <- deprivation_data %>% clean_names()
 head(deprivation_data) #I need to change the simd column to numeric otherwise it will be contiunious and we dont want that for the graph
 tail(deprivation_data)
@@ -16,14 +16,14 @@ deprivation_data <- deprivation_data %>%
   mutate(simd = as.factor(simd))
 
 #looking at the total count of each simd
-deprivation_data %>%
-  group_by(simd) %>%
-  summarise(count = n())
+simd_graph <- deprivation_data %>%
+  group_by(quarter, simd) %>%
+  summarise(stays = sum(stays))
 
 #making a set which looks at quarter and simd and groups them
-simd_quarter <- deprivation_data %>%
+simd_quarter_graph <- deprivation_data %>%
   group_by(quarter) %>%
-  count(simd)
+  mutate()
 
 
 #plotting each simd level and the total count in each qurter into a facet wrap
@@ -54,4 +54,21 @@ simd_quarter %>%
     subtitle = "Deprivation levels: 1(Most Deprived) - 5(Least Deprived)\n"
   )
 
+#------------ edited graph
 
+simd_graph %>%
+  ggplot() +
+  aes(x = quarter, y = stays, colour = as.factor(simd), group = simd) +
+  geom_line() +
+  scale_color_manual(values = cbbPalette) +
+  scale_y_continuous(labels = scales::comma) +
+  theme_light() +
+  theme(axis.text.x = element_text(angle = 90)) +
+  labs(
+    x = "\nQuarter and Year",
+    y = "Count of stays in each SIMD\n",
+    colour = "SIMD",
+    title = "The count of individuals in each SIMD across 2016 Q2 -2021 Q2",
+    subtitle = "Deprivation levels: 1(Most Deprived) - 5(Least Deprived)\n"
+  )
+  
